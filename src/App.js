9a420b842768ed5170613f77f2bb98eb2be475ff
christykazi <christyagbai4178@gmail.com>
2024-04-06 23:54:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-   <div></div>
+    <div>
+   <ol><ul><h3>travel website</h3></ul></ol>
+   <div className="hget">traveling site</div>
     </div>
   );
 }
