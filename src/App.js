63fa77d8b9c9784a2235d77ce132e342f076b384
christykazi <christyagbai4178@gmail.com>
2024-04-23 23:45:20 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
    <ol><ul><h3>travel website</h3></ul></ol>
    <div className="hget">traveling site</div>
    <div className="section">sect2</div>
-   <h4>trbel</h4>
+   <h4>travel</h4>
     </div>
 
   );
