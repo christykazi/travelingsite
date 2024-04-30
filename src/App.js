651d8cc6af50143a18ae852441c4ca1b5a10ed89
christykazi <christyagbai4178@gmail.com>
2024-04-30 22:43:01 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-   <ol><ul><h3>travel website</h3></ul></ol>
-   <div className="hget">traveling site</div>
-   <div className="section">sect2</div>
-   <h4>travel</h4>
+  
     </div>
 
   );
