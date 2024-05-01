8fb9601b1f6e39132c444db7e4d0aa371b9735f1
christykazi <christyagbai4178@gmail.com>
2024-05-01 01:30:47 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Main from './Components/Main/Main';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div>
-  <h1>Travel</h1>
-    </div>
+    <>
+    <Navbar/> 
+   {/*  <Home/> 
+    <Main/>
+    <Footer/> */}
+    </>
 
   );
 }
