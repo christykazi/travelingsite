b47a24dc023d37f7ae27ec51be25004edfc397b2
christykazi <christyagbai4178@gmail.com>
2024-05-03 23:20:57 +0100

    error

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import './main.css'
 
 const Main = () => {
   return (
-    <div>Main</div>
+  <section className='main container section'>
+<div className="secTitle">
+  <h3 className="title">
+    Most visted destinations
+  </div>
+</div>
+  </section>
   )
 }
 
