b214340780b5a5f8c40b12ff4fb57bb985d17a26
christykazi <christyagbai4178@gmail.com>
2024-05-05 01:10:57 +0100

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <section className="footer">
+        <div className="videoDiv">
+          <video src="https://cdn.pixabay.com/video/2021/02/18/65560-515098344_tiny.mp4" loop autoPlay muted type="video/mp4">
+          </video>
+        </div>
+        <div className="seContent container">
+          <div className="contactDiv flex">
+          <div className="text">
+            <small></small>
+          </div>  
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
